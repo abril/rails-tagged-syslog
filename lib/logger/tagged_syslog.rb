9e3db6b29100abc9402d4715f1b24e1c81741730
Giuliano Melo <giuliano.melo@abril.com.br>
2015-04-01 17:38:01 -0300

    :scissors: this should not be here

--- lib/logger/tagged_syslog.rb
+++ lib/logger/tagged_syslog.rb
@@ -6,8 +6,6 @@ module Abril
 
     def self.new(logger = nil)
       logger ||= ActiveSupport::TaggedLogging.new(Abril::Syslogger.new)
-      binding.pry
-      Rails.application.config.log_tags = [:uuid]
       logger
     end
   end
